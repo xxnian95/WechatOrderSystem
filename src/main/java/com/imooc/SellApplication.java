aea9fb91a320b9be62fe2f8632502a006927a7c9
Zhang Pengnian <zhangpengnian95@hotmail.com>
2019-10-10 00:15:14 +0800

    新建项目

---
+++ src/main/java/com/imooc/SellApplication.java
@@ -0,0 +1,16 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Zhang Pengnian
+ * @since 2019-10-09 22:47
+ */
+@SpringBootApplication
+public class SellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellApplication.class, args);
+    }
+}
